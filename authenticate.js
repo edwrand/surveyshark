4e905e09a290e535c63725f0f37ac155e4aa9f9a
edwrand <ebbyrandall@yahoo.com>
2023-04-09 20:33:07 -0500

    formatted index page and added some template surveys

--- authenticate.js
+++ authenticate.js
@@ -1,2 +1,11 @@
 // use a phone number to authenticate a user unqiue to the app
 // this is a one time use code type beat
+
+// user must have a phone number to sign up
+
+
+// user gets text code each time they log in, or withdraw money
+
+// only 1 user can have 1 phone number
+
+// success message that user has been logged in
